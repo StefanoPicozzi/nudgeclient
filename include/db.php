a8aabb4deb4cf2d2b3ae798902bf823d4abf5c87
Stefano Picozzi <stefanopicozzi@Stefanos-MacBook-Pro.local>
2015-12-09 20:46:57 +1100

    x

--- include/db.php
+++ include/db.php
@@ -1,61 +1,55 @@
 <?php
 
-define('DB_HOST', '');
-define('DB_PORT', '');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
-  
-define('CLIENT_GEAR_NAME', getenv('OPENSHIFT_GEAR_NAME'));
-define('MSQL_SERVICE_HOST', getenv('MYSQL_SERVICE_HOST'));
-
-$client_gear_name = CLIENT_GEAR_NAME;
-$mysql_service_host = MSQL_SERVICE_HOST;
+$client_gear_name = getenv('OPENSHIFT_GEAR_NAME');
+$mysql_service_host = getenv('MYSQL_SERVICE_HOST');
 
 if (! empty($client_gear_name) ) {
   
   #define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
   #define('DB_HOST', 'nudgeserver-spicozzi.rhcloud.com');
   #define('DB_HOST', '533673c04382eca3dd00061f-spicozzi.rhcloud.com');
-  define('DB_HOST', '535239b54382ec5cff00005c-spicozzi.rhcloud.com');
+  #define('DB_HOST', '535239b54382ec5cff00005c-spicozzi.rhcloud.com');
   
   #define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
   #define('DB_PORT', '53396');
-  define('DB_PORT', '44871');
+  #define('DB_PORT', '44871');
 
   #define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
   #define('DB_USER', 'adminXZyb64C');
-  define('DB_USER', 'admins4SFfJZ');
+  #define('DB_USER', 'admins4SFfJZ');
 
   #define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
   #define('DB_PASS', 'Xt4t23b_gLSt');
-  define('DB_PASS', 'IXpNxqRhElea');
+  #define('DB_PASS', 'IXpNxqRhElea');
 
   #define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
   #define('DB_NAME', 'nudgeserver');
   #define('DB_NAME', 'nudgedb');
-  define('DB_NAME', '');
+  #define('DB_NAME', '');
 
-} 
-else if (! empty($mysql_service_host) ) {
+  $mysql_hostname = '535239b54382ec5cff00005c-spicozzi.rhcloud.com';                               
+  $mysql_port     = '44871';                               
+  $mysql_username = 'admins4SFfJZ';                                        
+  $mysql_dbname   = '';                                   
+  $mysql_password = 'IXpNxqRhElea';                                   
 
-  define('DB_HOST', getenv("MYSQL_SERVICE_HOST"));
-  define('DB_PORT', getenv("MYSQL_SERVICE_PORT"));
-  define('DB_USER', getenv("DATABASE_USER"));
-  define('DB_NAME', getenv("DATABASE_NAME"));
-  define('DB_PASS', getenv("DATABASE_PASSWORD"));
-  
+} else if (! empty($mysql_service_host) ) {
+
+  $mysql_hostname = getenv('MYSQL_SERVICE_HOST');                               
+  $mysql_port     = getenv('MYSQL_SERVICE_PORT');                               
+  $mysql_username = getenv('MYSQL_USER');                                        
+  $mysql_dbname   = getenv('MYSQL_DATABASE');                                   
+  $mysql_password = getenv('MYSQL_PASSWORD');                                   
+                                                 
 }
 
 /*** connect to database ***/
 /*** mysql hostname ***/
-$mysql_hostname = DB_HOST;
 if (empty($mysql_hostname)) {
    $mysql_hostname = '127.0.0.1';
 }
 
 /*** mysql username ***/
-$mysql_username = DB_USER;
 if (empty($mysql_username)) {
   $mysql_username = 'root';
   #$mysql_username = 'adminXZyb64C';
@@ -63,15 +57,13 @@ if (empty($mysql_username)) {
 }
 
 /*** mysql password ***/
-$mysql_port = DB_PORT;
 if (empty($mysql_port)) {
-   $mysql_port = '3307';
+   $mysql_port = '3306';
    #$mysql_port = '53396';
    #$mysql_port = '44871';
 }
 
 /*** mysql password ***/
-$mysql_password = DB_PASS;
 if (empty($mysql_password)) {
    $mysql_password = '';
    #$mysql_password = 'Xt4t23b_gLSt';
@@ -79,7 +71,6 @@ if (empty($mysql_password)) {
 }
 
 /*** database name ***/
-$mysql_dbname = DB_NAME;
 if (empty($mysql_dbname)) {
    #$mysql_dbname = 'nudge';
    #$mysql_dbname = 'nudgeserver';
